cc5413e04842c22ece5f9c104afeb2d20900fceb
Justin Ferrell <jferrell826@gmail.com>
2017-01-26 08:28:56 -0800

    Update Warmup-1.java

--- Warmup-1.java
+++ Warmup-1.java
@@ -464,7 +464,8 @@ public String endUp(String str) {
 
 ----------------------------------------------------------------------------------------------------------------------------
 
-//Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+//Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string. 
+//So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
 
 //everyNth("Miracle", 2) → "Mrce"
 //everyNth("abcdefg", 2) → "aceg"
