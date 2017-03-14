203e26a66da387edded4388ff1cade0a0bd419d1
Justin Ferrell <jferrell826@gmail.com>
2017-03-13 19:48:58 -0700

    Incomplete: 2 Exercises Remaining

--- Map-2.java
+++ Map-2.java
@@ -162,3 +162,23 @@ public Map<String, Boolean> wordMultiple(String[] strings) {
 }
 
 ----------------------------------------------------------------------------------------------------------------------------
+
+/*
+We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2 strings in the array. When a position in the array has been swapped, it no longer matches anything. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+
+allSwap(["ab", "ac"]) → ["ac", "ab"]
+allSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) → ["ay", "by", "cy", "cx", "bx", "ax", "azz", "aaa"]
+allSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) → ["ay", "by", "ax", "bx", "aj", "ai", "by", "bx"]
+*/
+  
+----------------------------------------------------------------------------------------------------------------------------
+  
+/*
+We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2 strings in the array. A particular first char can only cause 1 swap, so once a char has caused a swap, its later swaps are disabled. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+
+firstSwap(["ab", "ac"]) → ["ac", "ab"]
+firstSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) → ["ay", "by", "cy", "cx", "bx", "ax", "aaa", "azz"]
+firstSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) → ["ay", "by", "ax", "bx", "ai", "aj", "bx", "by"]
+*/
+  
+----------------------------------------------------------------------------------------------------------------------------
