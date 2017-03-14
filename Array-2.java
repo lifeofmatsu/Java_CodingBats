c3ed06e05ac21f35f7dc71eb40a10432b72a55c5
Justin Ferrell <jferrell826@gmail.com>
2017-03-13 19:03:53 -0700

    Complete: All Exercises Uploaded

--- Array-2.java
+++ Array-2.java
@@ -112,6 +112,20 @@ public int sum13(int[] nums) {
 //sum67([1, 2, 2, 6, 99, 99, 7]) → 5
 //sum67([1, 1, 6, 7, 2]) → 4
 
+public int sum67(int[] nums) {
+  int sum = 0;
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] != 6) {
+      sum += nums[i];
+    } else {
+      while (nums[i] != 7) {
+        i++;
+      }
+    }
+  }
+  return sum;
+}
+
 ---------------------------------------------------------------------------------------------------------------------------
 
 //has22
@@ -311,6 +325,250 @@ public int[] shiftLeft(int[] nums) {
   }
   nums[nums.length - 1] = temp;
   return nums;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+
+/*
+For each multiple of 10 in the given array, change all the values following it to be that multiple of 10, 
+until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+
+tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
+tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
+*/
+  
+public int[] tenRun(int[] nums) {
+  int value = -1;
+  int[] array = new int[nums.length];
+  
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] % 10 == 0) {
+      value = nums[i];
+      array[i] = value;
+    } else if(value == -1) {
+      array[i] = nums[i];
+    } else {
+      array[i] = value;
+    }
+  }
+  return array;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+Given a non-empty array of ints, return a new array containing the elements from the original array that come before the 
+first 4 in the original array. The original array will contain at least one 4. Note that it is valid in java to create 
+an array of length 0.
+
+pre4([1, 2, 4, 1]) → [1, 2]
+pre4([3, 1, 4]) → [3, 1]
+pre4([1, 4, 4]) → [1]
+*/
+  
+public int[] pre4(int[] nums) {
+  int count = 0;
+  for(int i: nums) {
+    if(i == 4) break;
+    count++;
+  }
+  int [] array = new int [count];
+  for(int i = 0; i < count; i++) {
+    array[i] = nums[i];
+  }
+  return array;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+Given a non-empty array of ints, return a new array containing the elements from the original array that come after 
+the last 4 in the original array. The original array will contain at least one 4. Note that it is valid in java to 
+create an array of length 0.
+
+post4([2, 4, 1, 2]) → [1, 2]
+post4([4, 1, 4, 2]) → [2]
+post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+*/
+  
+public int[] post4(int[] nums) {
+  int last4 = 0;
+  for (int i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] == 4) {
+      last4 = i;
+      break;
+    }
+  }
+  int[] temp = new int[nums.length - 1 - last4];
+  int ctr1 = 0;
+  for (int i = last4 + 1; i < nums.length; i++){
+    temp[ctr1] = nums[i];
+    ctr1++;
+  }
+  return temp;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+We'll say that an element in an array is "alone" if there are values before and after it, and those values are different 
+from it. Return a version of the given array where every instance of the given value which is alone is replaced 
+by whichever value to its left or right is larger.
+
+notAlone([1, 2, 3], 2) → [1, 3, 3]
+notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
+notAlone([3, 4], 3) → [3, 4]
+*/
+  
+public int[] notAlone(int[] nums, int val) {
+  for (int i = 1; i < nums.length - 1; i++)
+    if (nums[i] == val && nums[i - 1] != val && nums[i + 1] != val)
+      nums[i] = Math.max(nums[i - 1], nums[i + 1]);
+    return nums;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+Return an array that contains the exact same numbers as the given array, but rearranged so that all the zeros are grouped 
+at the start of the array. The order of the non-zero numbers does not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. 
+You may modify and return the given array or make a new array.
+
+zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
+zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
+zeroFront([1, 0]) → [0, 1]
+*/
+  
+public int[] zeroFront(int[] nums) {
+  int count = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      nums[i] = nums[count];
+      nums[count] = 0;
+      count++;
+    }
+  }
+  return nums;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+Return a version of the given array where all the 10's have been removed. The remaining elements should shift left towards 
+the start of the array as needed, and the empty spaces a the end of the array should be 0. So {1, 10, 10, 2} yields 
+{1, 2, 0, 0}. You may modify and return the given array or make a new array.
+
+withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
+withoutTen([10, 2, 10]) → [2, 0, 0]
+withoutTen([1, 99, 10]) → [1, 99, 0]
+*/
+  
+public int[] withoutTen(int[] nums) {
+  int [] a = new int[nums.length];
+  int count = 0;
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] != 10) {
+      a[count] = nums[i];
+      count++;
+    }
+  }
+  for(int i = count; i < nums.length; i++) {
+    a[i] = 0;
+  }
+  return a;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+Return a version of the given array where each zero value in the array is replaced by the largest odd value to the right of 
+the zero in the array. If there is no odd value to the right of the zero, leave the zero as a zero.
+
+zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
+zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
+zeroMax([0, 1, 0]) → [1, 1, 0]
+*/
+  
+public int[] zeroMax(int[] nums) {
+  int maxOdd = -1;
+  for (int i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] != 0) {
+      if ((nums[i] >= maxOdd) && (nums[i] % 2 == 1)){
+        maxOdd = nums[i];
+      }
+    } else {
+      if (maxOdd != -1) {
+        nums[i] = maxOdd;
+      }
+    }
+  }
+  return nums;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+Return an array that contains the exact same numbers as the given array, but rearranged so that all the even numbers come 
+before all the odd numbers. Other than that, the numbers can be in any order. You may modify and return the given array, 
+or make a new array.
+
+evenOdd([1, 0, 1, 0, 0, 1, 1]) → [0, 0, 0, 1, 1, 1, 1]
+evenOdd([3, 3, 2]) → [2, 3, 3]
+evenOdd([2, 2, 2]) → [2, 2, 2]
+*/
+  
+public int[] evenOdd(int[] nums) {
+  int [] a = new int[nums.length];
+  int count = 0;
+  
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 == 0) {
+      a[count] = nums[i];
+      count++;
+    }
+  }
+  for(int i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 != 0) {
+      a[count] = nums[i];
+      count++;
+    }
+  }
+  return a;
+}
+
+---------------------------------------------------------------------------------------------------------------------------
+  
+/*
+This is slightly more difficult version of the famous FizzBuzz problem which is sometimes given as a first problem for 
+job interviews. (See also: FizzBuzz Code.) Consider the series of numbers beginning at start and running up to but not 
+including end, so for example start=1 and end=5 gives the series 1, 2, 3, 4. Return a new String[] array containing the 
+string form of these numbers, except for multiples of 3, use "Fizz" instead of the number, for multiples of 5 use "Buzz", 
+and for multiples of both 3 and 5 use "FizzBuzz". In Java, String.valueOf(xxx) will make the String form of an int or other 
+type. This version is a little more complicated than the usual version since you have to allocate and index into an array 
+instead of just printing, and we vary the start/end instead of just always doing 1..100.
+
+fizzBuzz(1, 6) → ["1", "2", "Fizz", "4", "Buzz"]
+fizzBuzz(1, 8) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7"]
+fizzBuzz(1, 11) → ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"]
+*/
+  
+public String[] fizzBuzz(int start, int end) {
+  String [] fb = new String[end - start];
+  int count = 0;
+  
+  for(int i = start; i < end; i++) {
+    if(i % 3 == 0 && i % 5 == 0) 
+      fb[count] = "FizzBuzz";
+    else if(i % 3 == 0) 
+      fb[count] = "Fizz";
+    else if(i % 5 == 0) 
+      fb[count] = "Buzz";
+    else 
+      fb[count] = String.valueOf(i);
+    count++;
+  }
+  return fb;
 }
 
 ---------------------------------------------------------------------------------------------------------------------------
