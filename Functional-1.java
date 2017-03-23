2219b2f202381cdcc73be1944217f9b8e21eeaf0
Justin Ferrell <jferrell826@gmail.com>
2017-03-23 09:45:21 -0700

    Complete: All Exercises Uploaded

--- Functional-1.java
+++ Functional-1.java
@@ -3,7 +3,7 @@ Given a list of integers, return a list where each integer is multiplied by 2.
 
 doubling([1, 2, 3]) → [2, 4, 6]
 doubling([6, 8, 6, 8, -1]) → [12, 16, 12, 16, -2]
-doubling([]) → []
+doubling([]) → [] 
 */
 
 public List<Integer> doubling(List<Integer> nums) {
