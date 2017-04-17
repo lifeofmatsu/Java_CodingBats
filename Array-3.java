736555f3c9d49f28145dfc10ea4cf8a4bb6e1ff3
Justin Ferrell <jferrell826@gmail.com>
2017-04-17 09:28:08 -0700

    Complete: All Exercises Uploaded

--- Array-3.java
+++ Array-3.java
@@ -213,7 +213,26 @@ maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
 maxMirror([1, 2, 1, 4]) → 3
 maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2
 */
-  
+
+public int maxMirror(int[] nums) {
+  int max = 0;
+  for (int i = 0; i < nums.length; i++) { //i will serve as the start of the array
+    int cnt = 0; //will count lengths of mirrored elements
+    for (int j = nums.length - 1; j >= 0; j--) { //j will serve as the 'end' of the array
+      // if (cnt + i < nums.length) //
+        if (cnt + i < nums.length && nums[i + cnt] == nums[j]) { 
+          cnt++;
+          if (max > cnt); //checks for larger group of contiguous elements
+          else
+            max = cnt;
+        } else if (cnt > 0) {
+          cnt = 0;
+        }
+    }
+  }
+  return max;
+}
+
 -----------------------------------------------------------------------------------------------------------------------------
   
 /*
